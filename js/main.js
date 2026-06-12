d210bd477e393fac0a47964f1d66e7577de02c81
WCH-WHY0426\user <ianmackenzie0909@gmail.com>
2026-06-13 00:22:52 +0800

    Edit links content

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ const translations = {
             optimization: 'Optimization'
         },
         project: {
-            live: 'Open live site',
+            live: 'Visit site',
             open: 'Open repository',
             asr: {
                 title: 'ASR-AMR: ROS 2 Self-Driving Vehicle Workspace',
@@ -177,7 +177,7 @@ const translations = {
             optimization: '最佳化'
         },
         project: {
-            live: '開啟正式網站',
+            live: '造訪網站',
             open: '開啟 repo',
             asr: {
                 title: 'ASR-AMR：ROS 2 自走車工作區',
